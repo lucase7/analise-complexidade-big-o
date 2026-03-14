e1e38979824371e033c8a3dfeb696f18e29ff9c7
Lucas Eduardo <165400101+lucase7@users.noreply.github.com>
2026-03-14 19:04:35 -0300

    Adiciona exercício 1 - função verificar_primeiro

--- exercicio1.py
+++ exercicio1.py
@@ -4,9 +4,4 @@ def verificar_primeiro(lista):
     return lista[0]
 
 
-# exemplo de execução
-numeros = [10, 20, 30, 40]
 
-resultado = verificar_primeiro(numeros)
-
-print("Primeiro elemento:", resultado)
